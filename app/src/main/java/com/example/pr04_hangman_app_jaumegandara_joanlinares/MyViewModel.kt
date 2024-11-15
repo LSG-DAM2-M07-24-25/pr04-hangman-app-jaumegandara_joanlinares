69379446239b8019d1a2cc32dab8b96c3419b7a1
JaumeGandara <jaumegandara@gmail.com>
2024-11-15 17:49:04 +0100

    fix commit

--- app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/MyViewModel.kt
+++ app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/MyViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.pr04_hangman_app_jaumegandara_joanlinares
 
-class MyViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class MyViewModel: ViewModel() {
+    var myVar: Boolean by mutableStateOf(false)
+        private set
+
+    fun modifyMyVar(value: Boolean) {
+        myVar = value
+    }
 }
