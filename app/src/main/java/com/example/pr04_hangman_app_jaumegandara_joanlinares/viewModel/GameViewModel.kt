007bca6427e32b051040ef61653fc1fb5ed8ebd8
JaumeGandara <jaumegandara@gmail.com>
2024-12-07 16:01:08 +0100

    feat: Visual del joc funcional, però sense comprovacions de si és correcte o no

--- app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/viewModel/GameViewModel.kt
+++ app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/viewModel/GameViewModel.kt
@@ -5,16 +5,15 @@ import androidx.lifecycle.ViewModel
 import com.example.pr04_hangman_app_jaumegandara_joanlinares.Model.Words
 
 class GameViewModel : ViewModel() {
-    val selectedWord = Words().getRandomWords("Easy")  // Exemple de dificultat "Easy"
-    val remainingAttempts = mutableStateOf(6)  // Comença amb 6 intents
-    val guessedLetters = mutableStateOf(mutableListOf<Char>())  // Lletres encertades per l'usuari
+    val selectedWord = Words().getRandomWords("Easy")
+    val remainingAttempts = mutableStateOf(6)
+    val guessedLetters = mutableStateOf(mutableListOf<Char>())
 
-    // Funció per manejar el clic a una lletra
     fun onLetterClicked(letter: Char) {
         if (letter in selectedWord) {
-            guessedLetters.value.add(letter)  // Afegir la lletra encertada
+            guessedLetters.value.add(letter)
         } else {
-            remainingAttempts.value -= 1  // Restar intents
+            remainingAttempts.value -= 1
         }
     }
 }
