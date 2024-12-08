7cbe8edde1cf82a32b8a704b3d332ce4589c7f3e
JoanLinares <joan.linares@gracia.lasalle.cat>
2024-12-08 11:46:32 +0100

    fix: Fixed traspassing difficulty to gameView

--- app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/view/EntryPoint.kt
+++ app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/view/EntryPoint.kt
@@ -1,11 +1,14 @@
 package com.example.pr04_hangman_app_jaumegandara_joanlinares.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.pr04_hangman_app_jaumegandara_joanlinares.Routes
+import com.example.pr04_hangman_app_jaumegandara_joanlinares.viewModel.GameViewModel
+import com.google.android.libraries.mapsplatform.transportation.consumer.model.Route
 
 @Composable
 fun EntryPoint(navigationController: NavController) {
@@ -15,7 +18,11 @@ fun EntryPoint(navigationController: NavController) {
     ) {
         composable(Routes.Screen1.route) { MenuScreen(navigationController) }
 
-        composable(Routes.Screen2.route) { GameScreen(navigationController) }
+        composable(Routes.Screen2.route) { backStackEntry ->
+            val difficulty = backStackEntry.arguments?.getString("difficulty") ?: "Medium"
+            val gameViewModel = remember { GameViewModel(difficulty) }
+            GameScreen(navigationController, gameViewModel)
+        }
 
         composable(Routes.Screen3.route) { ResultScreen(navigationController) }
     }
--- app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/viewModel/GameViewModel.kt
+++ app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/viewModel/GameViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.pr04_hangman_app_jaumegandara_joanlinares.viewModel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.pr04_hangman_app_jaumegandara_joanlinares.Model.Words
 
-class GameViewModel : ViewModel() {
-    val selectedWord = Words().getRandomWords("Easy")
-    val remainingAttempts = mutableStateOf(6)
+
+class GameViewModel(difficulty: String) : ViewModel() {
+    val selectedWord = Words().getRandomWords(difficulty)
+    val remainingAttempts = mutableIntStateOf(6)
+    @SuppressLint("MutableCollectionMutableState")
     val guessedLetters = mutableStateOf(mutableListOf<Char>())
 
     fun onLetterClicked(letter: Char) {
