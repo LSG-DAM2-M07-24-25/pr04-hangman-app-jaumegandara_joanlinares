6ace87ae092f0b133b92c9568bf9badf1c7b4c8c
JaumeGandara <jaumegandara@gmail.com>
2024-12-09 18:51:40 +0100

    feat: Final commit (colors and play again)

--- app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/view/ResultScreen.kt
+++ app/src/main/java/com/example/pr04_hangman_app_jaumegandara_joanlinares/view/ResultScreen.kt
@@ -4,7 +4,9 @@ package com.example.pr04_hangman_app_jaumegandara_joanlinares.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,9 +20,10 @@ import androidx.compose.ui.graphics.Color
 import com.example.pr04_hangman_app_jaumegandara_joanlinares.Routes
 
 @Composable
-fun ResultScreen(navController: NavController, isGameWon: Boolean, attempts: Int) {
-    val resultText = if (isGameWon) "¡Ganaste!" else "¡Perdiste!"
+fun ResultScreen(navController: NavController, isGameWon: Boolean, attempts: Int, difficulty: String) {
+    val resultText = if (isGameWon) "Congratulations!" else "¡Sorry!"
     val resultColor = if (isGameWon) Color.Green else Color.Red
+    val resultDescription = if (isGameWon) "You have succeeded after $attempts attempts" else "You have failed $attempts attempts"
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -30,17 +33,34 @@ fun ResultScreen(navController: NavController, isGameWon: Boolean, attempts: Int
         Text(
             text = resultText,
             color = resultColor,
-            fontSize = 24.sp,
+            fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp)
         )
         Text(
-            text = "Intentos Fallidos: $attempts",
-            fontSize = 18.sp,
+            text = resultDescription,
+            fontSize = 24.sp,
             modifier = Modifier.padding(16.dp)
         )
-        Button(onClick = { navController.navigate(Routes.Screen1.route) }) {
-            Text(text = "Volver al menú")
+        Button(
+            modifier = Modifier.padding(16.dp).width(350.dp).height(75.dp),
+            onClick = { navController.navigate(Routes.Screen2.createRoute(difficulty)) }
+        ) {
+            Text(
+                text = "Play again",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Button(
+            modifier = Modifier.padding(16.dp).width(350.dp).height(75.dp),
+            onClick = { navController.navigate(Routes.Screen1.route) }
+        ) {
+            Text(
+                text = "Menu",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
